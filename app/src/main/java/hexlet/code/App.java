e687c037f101c259734b1cb1307f32a74a0bb36b
UlianaPythonBackendDeveloper <Uliana_Python_dev@hotmail.com>
2026-08-12 23:07:08 +0300

    Add CLI greeting and Makefile run-dist target

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,7 @@
 package hexlet.code;
 
 public class App {
-    public String getGreeting() {
-        return "Welcome to the Brain Games!";
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+public static void main(String[] args){
+    Cli.greeting();
+}
 }
